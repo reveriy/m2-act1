d48d4f4a617b4a5d3ee420299d4e910fc7ee97c2
Valorie Portem <valorieportem@gmail.com>
2024-10-07 20:26:44 +0800

    initial commit

---
+++ ageCheck.js
@@ -0,0 +1,10 @@
+let Age = 6;
+if (Age <= 13){
+    console.log("You are a child.");
+
+} else if(Age >=19) {
+    console.log("You are a teenager.");
+
+} else {
+    console.log("You are an adult.");
+}
