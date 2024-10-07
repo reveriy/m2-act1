adbf0e63cec8a40d6818210bd35c39063369e4fa
Valorie Portem <valorieportem@gmail.com>
2024-10-07 21:32:06 +0800

    git commit -m "initial commit"

--- ageCheck.js
+++ ageCheck.js
@@ -1,10 +1,9 @@
 let age = prompt("Enter your age:");
-age = parseInt(age);
 
 if (age < 13) {
-  console.log("You are a child.");
+  alert("You are a child.");
 } else if (age >= 13 && age <= 19) {
-  console.log("You are a teenager.");
+  alert("You are a teenager.");
 } else {
-  console.log("You are an adult.");
+  alert("You are an adult.");
 }
